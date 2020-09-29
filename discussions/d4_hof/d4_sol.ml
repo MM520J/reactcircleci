c2f3f70dcedda407fee0b39faec51e00f1ca0017
MM520J <108415228+MM520J@users.noreply.github.com>
2020-09-29 16:40:01 +0000

    sound control: WCD9320: update speaker gain control for Z1

--- discussions/d4_hof/d4_sol.ml
+++ discussions/d4_hof/d4_sol.ml
@@ -1,3 +1,35 @@
 
 type 'a tree = 
   | Leaf 
+  | Node of 'a tree * 'a * 'a tree
+
+let rec map f xs = 
+  match xs with
+      [] -> []
+    | h::t -> (f h)::(map f t)
+
+let rec fold f a lst =
+    match lst with
+    []->a
+    |h::t->fold f (f a h) t
+
+let rec fold_right f lst a =
+    match lst with
+    []->a
+    |h::t-> f h (fold_right f t a)
+
+let rec map_tree f t =
+  match t with
+  | Leaf           -> Leaf
+  | Node (l, v, r) -> let new_l = map_tree f l in
+      let new_r = map_tree f r in
+      Node (new_l, f v, new_r)
+
+
+let rec fold_tree f b t =
+  match t with
+  | Leaf           -> b
+  | Node (l, v, r) -> let res_l = fold_tree f b l in
+      let res_r = fold_tree f b r in
+      f res_l v res_r
+
