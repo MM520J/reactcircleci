35d456233b58278bba289fac9a4b25a82e5e1a18
MM520J <108415228+MM520J@users.noreply.github.com>
2021-03-02 07:37:02 +0000

    refs #2897

---
+++ projects/project1b/test/public/public.rb
@@ -0,0 +1,11 @@
+
+require "minitest/autorun"
+require_relative "../../src/translate.rb"
+
+# This is how you can load in files for testing
+WORDS_FILE1 = "#{__dir__}/inputs/words1.txt"
+GRAMMAR_FILE1 = "#{__dir__}/inputs/grammar1.txt"
+WORDS_FILE2 = "#{__dir__}/inputs/words2.txt"
+GRAMMAR_FILE2 = "#{__dir__}/inputs/grammar2.txt"
+WORDS_FILE3 = "#{__dir__}/inputs/words3.txt"
+GRAMMAR_FILE3 = "#{__dir__}/inputs/grammar3.txt"
